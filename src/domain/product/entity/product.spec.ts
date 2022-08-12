d199a1a4ece222a32cfb7a5db217288d24cf02ea
Dilthey Aislan Lopes de Paula <diltheyaislan@gmail.com>
2022-08-12 11:11:44 +0100

    add test to validate accumulated exceptions

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
     }).toThrowError("product: Price must be greater than zero");
   });
 
+  it("should throw error when name is empty and price is less than zero", () => {
+    expect(() => {
+		const product = new Product("123", "", -1);
+    }).toThrowError("product: Name is required,product: Price must be greater than zero");
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
